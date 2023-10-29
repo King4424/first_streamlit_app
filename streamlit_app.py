41eda60645d20c9e2ef66b7cc49c06fa42143130
Vaibhav Arun Khairnar <121480992+King4424@users.noreply.github.com>
2023-10-29 14:25:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,20 +4,16 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 streamlit.title('🥣My Parents New Healthy Diner')
- 
 streamlit.header('🥗Breakfast menu')
 streamlit.text('🐔omega 3 & blueberry Oatmeal')
 streamlit.text('🥑kale, Spinach & rocket smoothie')
 streamlit.text('🍞hard-boiled free-range egg')
- 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
  
 #import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 my_fruit_list = my_fruit_list.set_index('Fruit')
- 
- 
  
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
@@ -26,6 +22,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
  
+
+
+
+#create the repeatable code block (called a funcation)
+def get_fruityvice_data(this_fruit_choice):
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   return fruityvice_normalized   
 #New Section 
 streamlit.header("Fruityvice Fruit Advice!")
 try: 
@@ -33,10 +37,12 @@ try:
    if not fruit_choice:
      streamlit.error("Please select a fruit to get information.")
    else:
+     back_from_function = get_fruityvice_data(fruity_choice)
+     streamlit.dataframe(back_from_function)
 #import requests
-     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-     streamlit.dataframe(fruityvice_normalized)
+     #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+     #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+     #streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
    streamlit.error()
 # write your own comment -what does the next line do? 
